d036e84163c1f3fa50726c5e2ee7da5085a40119
Kwon Do Hee <doeesoioi3@gmail.com>
2025-05-26 14:22:08 +0900

    friends_list 작성

---
+++ friendsList.php
@@ -0,0 +1,16 @@
+<html> 
+    <head><title>친구목록</title></head>
+<body>
+    감정청<br><br>
+    친구가 있나요?<br>
+    <form action="/friends.php" method="post">
+        <label for="friend">Friends</label>
+  <select name="Friends List" id="friend">
+    <option value="권도희">권도희</option>
+    <option value="서혜랑">서혜랑</option>
+    <option value="안희진">안희진</option>
+  </select>
+  <input type="submit" value="Submit" />
+    </form>
+</body>
+</html>
--- main.php
+++ main.php
@@ -1,4 +1,17 @@
-<?php
-//우선순위 : 1순위위
+<html> 
+    <head><title>로그인</title></head>
+<body>
+    감정청<br><br>
+    감정 기록이 있나요?<br>
+    <form action="/request_signIn.php" method="post">
+        ID <Input type="text" name="id"><br>
+        PWD <Input type="password" name="password"><br><br>
+        <input type="submit" value="로그인 하기"><br><br>
+        <a href="signup.php" class="ca">일기 예보 작성하기</a>
+        <a href="signup.php" class="ca">일기 예보 상세보기</a>
+        <a href="signup.php" class="ca">감정청 방문하기</a>
+        <a href="frendsList.php" class="ca">이웃집</a>
+    </form>
+</body>
+</html>
 
-?>
